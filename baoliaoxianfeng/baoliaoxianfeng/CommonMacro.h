38177e188a976eb2236d54523c037373aa6ba3b2
Zhenhua Jiang <jiangzhenhua@123juzi.com>
2016-05-27 14:37:48 +0800

    添加pch文件和公用头文件

---
+++ baoliaoxianfeng/baoliaoxianfeng/CommonMacro.h
@@ -0,0 +1,18 @@
+//
+//  CommonMacro.h
+//  baoliaoxianfeng
+//
+//  Created by ios-yf on 16/5/27.
+//  Copyright © 2016年 OneManArmy. All rights reserved.
+//
+
+#ifndef CommonMacro_h
+#define CommonMacro_h
+
+#ifdef DEBUG
+# define DLog(fmt, ...) NSLog((@"[文件名:%s]\n" "[函数名:%s]\n" "[行号:%d] \n" fmt), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+# define DLog(...);
+#endif
+
+#endif /* CommonMacro_h */
--- baoliaoxianfeng/baoliaoxianfeng/Network/DJCustomURLProtocol.m
+++ baoliaoxianfeng/baoliaoxianfeng/Network/DJCustomURLProtocol.m
@@ -10,4 +10,5 @@
 
 @implementation DJCustomURLProtocol
 
+
 @end
