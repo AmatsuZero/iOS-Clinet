55bf14d6f91d3a6a493f85317c92567d035f42ee
Zhenhua Jiang <jiangzhenhua@123juzi.com>
2016-05-27 15:08:08 +0800

    添加了Masonry

--- baoliaoxianfeng/baoliaoxianfeng/CommonMacro.h
+++ baoliaoxianfeng/baoliaoxianfeng/CommonMacro.h
@@ -8,11 +8,13 @@
 
 #ifndef CommonMacro_h
 #define CommonMacro_h
-
+//自定义打印
 #ifdef DEBUG
 # define DLog(fmt, ...) NSLog((@"[文件名:%s]\n" "[函数名:%s]\n" "[行号:%d] \n" fmt), __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__);
 #else
 # define DLog(...);
 #endif
+//Masonry省略mas_开头
+#define MAS_SHORTHAND
 
 #endif /* CommonMacro_h */
